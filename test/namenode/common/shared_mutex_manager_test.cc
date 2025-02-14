ec899abf00bf399915f89b71b8935196d0f53cdc
Clcanny <a837940593@gmail.com>
2025-02-14 23:34:13 +0800

    t

--- test/namenode/common/shared_mutex_manager_test.cc
+++ test/namenode/common/shared_mutex_manager_test.cc
@@ -6,8 +6,16 @@
 
 #include <string>
 #include <string_view>
+#include <unifex/detail/with_type_erased_tag_invoke.hpp>
+#include <unifex/overload.hpp>
+#include <unifex/sender_for.hpp>
+#include <utility>
 
 namespace rocketfs {
+
+TEST(LockGuardTest, DefaultConstructedObjectHasTrivialDestructor) {
+  LockGuard<std::string, std::string_view> lock_guard;
+}
 
 TEST(SharedMutexManagerTest, Basic) {
   SharedMutexManager<std::string, std::string_view> manager(1);
